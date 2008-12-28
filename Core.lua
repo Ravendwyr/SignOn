d2844302cb757728ca51b81ac96c8dcf8bf20efb
Ethan Centaurai <ethan.centaurai@live.co.uk>
2008-12-28 18:04:59 +0000

    Comptletely rewritten! Now works with and without Prat-3.0!

--- Core.lua
+++ Core.lua
@@ -1,99 +1,8 @@
-﻿--[[
-Name: Prat SignOn
-Author: Ethan Centaurai (originally written by CipherSoft)
-Description: Adds extra player information to guild and friend signon/signoff messages
-Dependencies: Prat-3.0
-]]--
+﻿
+local SignOn = LibStub("AceAddon-3.0"):NewAddon("SignOn")
+local db, CLR
 
-local PRAT_MODULE = Prat:RequestModuleName("SignOn")
-if PRAT_MODULE == nil then return end
-
-local L = Prat:GetLocalizer({})
-
-L:AddLocale("enUS", {
-	["SignOn"] = true,
-	["Adds extra player information to guild and friend signon/signoff messages"] = true,
-
-	["Guild Signon String"] = true,
-	["Format string for when guild members sign on"] = true,
-	["Guild Signoff String"] = true,
-	["Format string for when guild members log off"] = true,
-	["Friend Signon String"] = true,
-	["Format string for when friends sign on"] = true,
-	["Friend Signoff String"] = true,
-	["Format string for when friends log off"] = true,
-
-	["Colour Names"] = true,
-	["Recolour player names in the message useing the colouring settings in the PlayerName module."] = true,
-
-	["String can contain any characters. Acceptable tags are: &name, &level, &class, &zone, &rank, &note"] = true,
-})
-
-
-local SignOn = Prat:NewModule(PRAT_MODULE)
-local db
-
-Prat:SetModuleDefaults(SignOn.name, {
-	profile = {
-		on = true,
-		colourNames = false,
-
-		guildOn = "<Guild> &rank &name [&level &class] has come online in &zone (Note: &note)",
-		guildOff = "<Guild> &rank &name [&level &class] has logged off (Note: &note)",
-		friendOn = "<Friend> &name [&level &class] has signed on in &zone (Note: &note)",
-		friendOff = "<Friend> &name [&level &class] has logged off (Note: &note)",
-	}
-})
-
-Prat:SetModuleOptions(SignOn, {
-	name = L["SignOn"],
-	desc = L["Adds extra player information to guild and friend signon/signoff messages"],
-	type = "group",
-	args = {
-		guildOn = {
-			name = L["Guild Signon String"],
-			desc = L["Format string for when guild members sign on"],
-			type = "input", order = 1,
-			usage = L["String can contain any characters. Acceptable tags are: &name, &level, &class, &zone, &rank, &note"],
-		},
-		guildOff = {
-			name = L["Guild Signoff String"],
-			desc = L["Format string for when guild members log off"],
-			type = "input", order = 2,
-			usage = L["String can contain any characters. Acceptable tags are: &name, &level, &class, &zone, &rank, &note"],
-		},
-		friendOn = {
-			name = L["Friend Signon String"],
-			desc = L["Format string for when friends sign on"],
-			type = "input", order = 3,
-			usage = L["String can contain any characters. Acceptable tags are: &name, &level, &class, &zone, &rank, &note"],
-		},
-		friendOff = {
-			name = L["Friend Signoff String"],
-			desc = L["Format string for when friends log off"],
-			type = "input", order = 4,
-			usage = L["String can contain any characters. Acceptable tags are: &name, &level, &class, &zone, &rank, &note"],
-		},
-		colourNames = {
-			name = L["Colour Names"],
-			desc = L["Recolour player names in the message useing the colouring settings in the PlayerName module."],
-			type = "toggle", order = 5,
-		},
-	},
-})
-
-
-function SignOn:OnModuleEnable()
-	Prat.RegisterChatEvent(self, "Prat_PreAddMessage")
-	db = self.db.profile
-end
-
-function SignOn:OnModuleDisable()
-	Prat.UnregisterAllChatEvents(self)
-end
-
-
-function SignOn:GetUserData(playerName)
+local function getUserData(playerName)
 	local u
 
 	-- Search guild
@@ -102,9 +11,7 @@ function SignOn:GetUserData(playerName)
 			u = { type = "GUILD" }
 			u.name, u.rank, _, u.level, u.class, u.zone, u.note, _, _, _ = GetGuildRosterInfo(i)
 
-			if playerName == u.name then
-				return u
-			end
+			if playerName == u.name then return u end
 		end
 	end
 
@@ -113,32 +20,28 @@ function SignOn:GetUserData(playerName)
 		u = { type = "FRIEND" }
 		u.name, u.level, u.class, u.zone, _, _, u.note = GetFriendInfo(i)
 
-		if playerName == u.name then
-			return u
-		end
+		if playerName == u.name then return u end
 	end
 end
 
-
-local CLR = Prat.CLR
-
-function SignOn:Prat_PreAddMessage(_, message, frame, event, t, r, g, b)
-	if event ~= "CHAT_MSG_SYSTEM" then return end -- only process system messages
-
+local function signOn(message, player)
 	local name, online
 
-	if message.MESSAGE:find("online") then -- user came online
-		name, online = message.PLAYER, true
+	if message:find("online") then -- user came online
+		name, online = message:match("|Hplayer:(%w+)|h.-|h has come online"), true
 
-	elseif message.MESSAGE:find("offline") then -- user went offline
-		name, online = message.MESSAGE:match("(%a+)"), false
+	elseif message:find("offline") then -- user went offline
+		name, online = message:match("(%w+) has gone offline"), false
 
 	else return end
 
-	name = name:gsub("|c%x%x%x%x%x%x%x%x", ""):gsub("|r", "") -- strip out colour codes, we'll add them back later
+	if not name then if player then name = player
+	else return end end
+
+	name = name:gsub("|c%x%x%x%x%x%x%x%x", ""):gsub("|r", "") -- strip out Prat-3.0 colour codes
 
 	-- have captured player name, and broken out of the function if this isnt the event we want.
-	local data = self:GetUserData(name)
+	local data = getUserData(name)
 	if not data then return end -- couldn't get information, don't alter the text.
 
 	local msg
@@ -157,14 +60,83 @@ function SignOn:Prat_PreAddMessage(_, message, frame, event, t, r, g, b)
 		end
 	end
 
-	if db.colourNames then
+	if db.colourNames and CLR then
 		data.name = CLR:Player(data.name, data.name, data.class)
 	end
 
-	data.name = "|Hplayer:"..name.."|h"..data.name.."|h" -- name is uncoloured, data.name is potentially coloured. coloured names kill the link if used between |Hplayer and the first |h
+	data.name = "|Hplayer:"..name.."|h"..data.name.."|h" -- name is uncoloured text, data.name is coloured text. coloured names kill the link if used between '|Hplayer' and the first '|h'
 
 	-- choo choo! here comes the gsub train!
 	msg = msg:gsub("&name", data.name):gsub("&level", tostring(data.level)):gsub("&class", data.class):gsub("&zone", data.zone or ""):gsub("&rank", data.rank or ""):gsub("&note", data.note or "")
+
+	return false, msg
+end
+
+
+function SignOn:OnEnable()
+	self.db = LibStub("AceDB-3.0"):New("SignOnDB", { profile = { colourNames = false,
+		guildOn = "<Guild> &rank &name [&level &class] has come online in &zone (Note: &note)",
+		guildOff = "<Guild> &rank &name [&level &class] has logged off (Note: &note)",
+		friendOn = "<Friend> &name [&level &class] has signed on in &zone (Note: &note)",
+		friendOff = "<Friend> &name [&level &class] has logged off (Note: &note)",
+	}}, "Default")
+
+	db = self.db.profile
+
+	if IsAddOnLoaded("Prat-3.0") then
+		Prat.RegisterChatEvent(self, "Prat_PreAddMessage")
+		CLR = Prat.CLR
+	else
+		ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", signOn)
+	end
+
+	LibStub("AceConfig-3.0"):RegisterOptionsTable("SignOn", {
+		name = "Sign On",
+		desc = "Allows you to customise guild and friend signon/signoff messages.",
+		type = "group",
+		get = function(key) return db[key.arg] end,
+		set = function(key, value) db[key.arg] = value end,
+		args = {
+			guildOn = {
+				name = "Guild Signon String",
+				desc = "Format string for when guild members sign on",
+				type = "input", order = 1, arg = "guildOn",
+				usage = "String can contain any characters. Acceptable tags are: &name, &level, &class, &zone, &rank, &note.",
+			},
+			guildOff = {
+				name = "Guild Signoff String",
+				desc = "Format string for when guild members log off",
+				type = "input", order = 2, arg = "guildOff",
+				usage = "String can contain any characters. Acceptable tags are: &name, &level, &class, &zone, &rank, &note.",
+			},
+			friendOn = {
+				name = "Friend Signon String",
+				desc = "Format string for when friends sign on",
+				type = "input", order = 3, arg = "friendOn",
+				usage = "String can contain any characters. Acceptable tags are: &name, &level, &class, &zone, &rank, &note.",
+			},
+			friendOff = {
+				name = "Friend Signoff String",
+				desc = "Format string for when friends log off",
+				type = "input", order = 4, arg = "friendOff",
+				usage = "String can contain any characters. Acceptable tags are: &name, &level, &class, &zone, &rank, &note.",
+			},
+			colourNames = {
+				name = "Colour Names",
+				desc = "Change the colour of player names. Requires Prat-3.0 to be installed.",
+				type = "toggle", order = 5, arg = "colourNames",
+				disabled = function() return not CLR end,
+			},
+		},
+	})
+
+	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("SignOn", "Sign On")
+end
+
+function SignOn:Prat_PreAddMessage(_, message, frame, event, t, r, g, b)
+	if event ~= "CHAT_MSG_SYSTEM" then return end
+
+	_, message.MESSAGE = signOn(message.MESSAGE, message.PLAYER)
 
 	-- nil out all message data except actual content.
 	-- we have to do this otherwise the players name will appear twice in the message.
@@ -177,7 +149,5 @@ function SignOn:Prat_PreAddMessage(_, message, frame, event, t, r, g, b)
 	message.Ll = ""
 	message.pP = ""
 	message.Pp = ""
-	message.Ff = ""
-	message.MESSAGE = msg
 end
 
