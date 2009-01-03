653a627bfb9d957579e42e93bffb7d5795c2f89a
Ethan Centaurai <ethan.centaurai@live.co.uk>
2009-01-03 22:13:09 +0000

    Export enUS translations from the WowAce Localiser.

--- enUS.lua
+++ enUS.lua
@@ -2,44 +2,36 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("SignOn", "enUS", true)
 if not L then return end
 
-L["Death Knight"] = true
-L["Druid"] = true
-L["Hunter"] = true
-L["Mage"] = true
-L["Paladin"] = true
-L["Priest"] = true
-L["Rogue"] = true
-L["Shaman"] = true
-L["Warrior"] = true
-L["Warlock"] = true
-
-L["has come online"] = true
-L["|Hplayer:(.-)|h.-|h has come online"] = true
-
-L["has gone offline"] = true
-L["(.-) has gone offline"] = true
-
-L["<Guild> &rank &name:random [&level &class:class] has come:green online:green in &zone (Note: &note)"] = true
-L["<Guild> &rank &name:random [&level &class:class] has logged:red off:red (Note: &note)"] = true
-L["<Friend> &name:random [&level &class:class] has signed:green on:green in &zone (Note: &note)"] = true
-L["<Friend> &name:random [&level &class:class] has logged:red off:red (Note: &note)"] = true
-
-L["Tutorial"] =	"Strings can contain tags, colour flags and any other characters.\n"..
-		"Acceptable tags are &name, &level, &class, &zone, &rank, and &note.\n"..
-		"Acceptable colour flags are :random, :class, :green, :red, :blue, :pink, :cyan, :yellow, and :orange.\n"..
-		":random follows the same rules as Prat-3.0's 'random' playername colouring setting.\n"..
-		"For example, &name:class would become %s.\n"..
-		"Anything else will be assumed to be part of the message."
-
-L["Guild Log-on Message"] = true
-L["Format string for when guild members sign on"] = true
-
-L["Guild Log-off Message"] = true
-L["Format string for when guild members log off"] = true
-
-L["Friend Log-on Message"] = true
-L["Format string for when friends sign on"] = true
-
-L["Friend Log-off Message"] = true
-L["Format string for when friends log off"] = true
+L["(.-) has gone offline"] = "(.-) has gone offline"
+L["<Friend> &name:random [&level &class:class] has logged:red off:red (Note: &note)"] = "<Friend> &name:random [&level &class:class] has logged:red off:red (Note: &note)"
+L["<Friend> &name:random [&level &class:class] has signed:green on:green in &zone (Note: &note)"] = "<Friend> &name:random [&level &class:class] has signed:green on:green in &zone (Note: &note)"
+L["<Guild> &rank &name:random [&level &class:class] has come:green online:green in &zone (Note: &note)"] = "<Guild> &rank &name:random [&level &class:class] has come:green online:green in &zone (Note: &note)"
+L["<Guild> &rank &name:random [&level &class:class] has logged:red off:red (Note: &note)"] = "<Guild> &rank &name:random [&level &class:class] has logged:red off:red (Note: &note)"
+L["|Hplayer:(.-)|h.-|h has come online"] = "|Hplayer:(.-)|h.-|h has come online"
+L["Death Knight"] = "Death Knight"
+L["Druid"] = "Druid"
+L["Format string for when friends log off"] = "Format string for when friends log off"
+L["Format string for when friends sign on"] = "Format string for when friends sign on"
+L["Format string for when guild members log off"] = "Format string for when guild members log off"
+L["Format string for when guild members sign on"] = "Format string for when guild members sign on"
+L["Friend Log-off Message"] = "Friend Log-off Message"
+L["Friend Log-on Message"] = "Friend Log-on Message"
+L["Guild Log-off Message"] = "Guild Log-off Message"
+L["Guild Log-on Message"] = "Guild Log-on Message"
+L["has come online"] = "has come online"
+L["has gone offline"] = "has gone offline"
+L["Hunter"] = "Hunter"
+L["Mage"] = "Mage"
+L["Paladin"] = "Paladin"
+L["Priest"] = "Priest"
+L["Rogue"] = "Rogue"
+L["Shaman"] = "Shaman"
+L["Tutorial"] = "Strings can contain tags, colour flags and any other characters.\
+Acceptable tags are &name, &level, &class, &zone, &rank, and &note.\
+Acceptable colour flags are :random, :class, :green, :red, :blue, :pink, :cyan, :yellow, and :orange.\
+:random follows the same rules as Prat-3.0's 'random' playername colouring setting.\
+For example, &name:class would become %s.\
+Anything else will be assumed to be part of the message."
+L["Warlock"] = "Warlock"
+L["Warrior"] = "Warrior"
 
