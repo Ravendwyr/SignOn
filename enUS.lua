f0133f941a75473ec62b5bfd758b666aa1b6222a
Ethan Centaurai <ethan.centaurai@live.co.uk>
2009-01-07 18:44:56 +0000

    Add ruRU localisations. Thanks zuz666!

--- enUS.lua
+++ enUS.lua
@@ -1,37 +1,47 @@
-﻿-- English localization file for enUS and enGB.
+﻿-- English localization file for enUS and enGB
 local L = LibStub("AceLocale-3.0"):NewLocale("SignOn", "enUS", true)
 if not L then return end
 
-L["(.-) has gone offline"] = "(.-) has gone offline"
-L["<Friend> &name:random [&level &class:class] has logged:red off:red (Note: &note)"] = "<Friend> &name:random [&level &class:class] has logged:red off:red (Note: &note)"
-L["<Friend> &name:random [&level &class:class] has signed:green on:green in &zone (Note: &note)"] = "<Friend> &name:random [&level &class:class] has signed:green on:green in &zone (Note: &note)"
-L["<Guild> &rank &name:random [&level &class:class] has come:green online:green in &zone (Note: &note)"] = "<Guild> &rank &name:random [&level &class:class] has come:green online:green in &zone (Note: &note)"
-L["<Guild> &rank &name:random [&level &class:class] has logged:red off:red (Note: &note)"] = "<Guild> &rank &name:random [&level &class:class] has logged:red off:red (Note: &note)"
-L["|Hplayer:(.-)|h.-|h has come online"] = "|Hplayer:(.-)|h.-|h has come online"
-L["Death Knight"] = "Death Knight"
-L["Druid"] = "Druid"
-L["Format string for when friends log off"] = "Format string for when friends log off"
-L["Format string for when friends sign on"] = "Format string for when friends sign on"
-L["Format string for when guild members log off"] = "Format string for when guild members log off"
-L["Format string for when guild members sign on"] = "Format string for when guild members sign on"
-L["Friend Log-off Message"] = "Friend Log-off Message"
-L["Friend Log-on Message"] = "Friend Log-on Message"
-L["Guild Log-off Message"] = "Guild Log-off Message"
-L["Guild Log-on Message"] = "Guild Log-on Message"
-L["has come online"] = "has come online"
-L["has gone offline"] = "has gone offline"
-L["Hunter"] = "Hunter"
-L["Mage"] = "Mage"
-L["Paladin"] = "Paladin"
-L["Priest"] = "Priest"
-L["Rogue"] = "Rogue"
-L["Shaman"] = "Shaman"
+L["(.-) has gone offline"] = true -- pattern
+L["<Friend> &name:random [&level &class:class] has logged:red off:red (Note: &note)"] = true
+L["<Friend> &name:random [&level &class:class] has signed:green on:green in &zone (Note: &note)"] = true
+L["<Guild> &rank &name:random [&level &class:class] has come:green online:green in &zone (Note: &note)"] = true
+L["<Guild> &rank &name:random [&level &class:class] has logged:red off:red (Note: &note)"] = true
+L["|Hplayer:(.-)|h.-|h has come online"] = true -- pattern
+L["Death Knight"] = true -- class male
+L["DEATH KNIGHT"] = true -- class female
+L["Druid"] = true -- class male  
+L["DRUID"] = true -- class female
+L["Format string for when friends log off"] = true -- tooltip
+L["Format string for when friends sign on"] = true -- tooltip
+L["Format string for when guild members log off"] = true -- tooltip
+L["Format string for when guild members sign on"] = true -- tooltip
+L["Friend Log-off Message"] = true -- options item
+L["Friend Log-on Message"] = true -- options item
+L["Guild Log-off Message"] = true -- options item
+L["Guild Log-on Message"] = true -- options item
+L["has come online"] = true -- pattern
+L["has gone offline"] = true -- pattern
+L["Hunter"] = true -- class male  
+L["HUNTER"] = true -- class female
+L["Mage"] = true -- class male  
+L["MAGE"] = true -- class female
+L["Paladin"] = true -- class male  
+L["PALADIN"] = true -- class female
+L["Priest"] = true -- class male  
+L["PRIEST"] = true -- class female
+L["Rogue"] = true -- class male  
+L["ROGUE"] = true -- class female
+L["Shaman"] = true -- class male  
+L["SHAMAN"] = true -- class female
 L["Tutorial"] = "Strings can contain tags, colour flags and any other characters.\
 Acceptable tags are &name, &level, &class, &zone, &rank, and &note.\
 Acceptable colour flags are :random, :class, :green, :red, :blue, :pink, :cyan, :yellow, and :orange.\
 :random follows the same rules as Prat-3.0's 'random' playername colouring setting.\
 For example, &name:class would become %s.\
 Anything else will be assumed to be part of the message."
-L["Warlock"] = "Warlock"
-L["Warrior"] = "Warrior"
+L["Warlock"] = true -- class male  
+L["WARLOCK"] = true -- class female
+L["Warrior"] = true -- class male  
+L["WARRIOR"] = true -- class female
 
---
+++ ruRU.lua
@@ -0,0 +1,47 @@
+﻿-- Russian localization file for ruRU by Yuris Auzinsh (Zuz666).
+local L = LibStub("AceLocale-3.0"):NewLocale("SignOn", "ruRU")
+if not L then return end
+
+L["(.-) has gone offline"] = "(.-) выходит из игрового мира." -- pattern
+L["<Friend> &name:random [&level &class:class] has logged:red off:red (Note: &note)"] = "<Друг> &name:random [&level &class:class] вышел:red (Примечание: &note)"
+L["<Friend> &name:random [&level &class:class] has signed:green on:green in &zone (Note: &note)"] = "<Друг> &name:random [&level &class:class] зашел:green в &zone (Примечание: &note)"
+L["<Guild> &rank &name:random [&level &class:class] has come:green online:green in &zone (Note: &note)"] = "<Гильдия> &rank &name:random [&level &class:class] зашел:green в &zone (Примечание: &note)"
+L["<Guild> &rank &name:random [&level &class:class] has logged:red off:red (Note: &note)"] = "<Гильдия> &rank &name:random [&level &class:class] вышел:red (Примечание: &note)"
+L["|Hplayer:(.-)|h.-|h has come online"] = "|Hplayer:(.-)|h.-|h входит в игру." -- pattern
+L["Death Knight"] = "Рыцарь смерти" -- class male
+L["DEATH KNIGHT"] = "Рыцарь смерти" -- class female
+L["Druid"] = "Друид" -- class male  
+L["DRUID"] = "Друид" -- class female
+L["Format string for when friends log off"] = "Шаблон сообщения для случая, когда друг выходит из игрового мира" -- tooltip
+L["Format string for when friends sign on"] = "Шаблон сообщения для случая, когда друг входит в игровой мир" -- tooltip
+L["Format string for when guild members log off"] = "Шаблон сообщения для случая, когда согильдиец входит в игровой мир" -- tooltip
+L["Format string for when guild members sign on"] = "Шаблон сообщения для случая, когда согильдиец выходит из игрового мира" -- tooltip
+L["Friend Log-off Message"] = "Сообщение выхода из игры друга" -- options item
+L["Friend Log-on Message"] = "Сообщение входа в игру друга"  -- options item
+L["Guild Log-off Message"] = "Сообщение выхода из игры согильдийца" -- options item
+L["Guild Log-on Message"] = "Сообщение выхода в игру согильдийца" -- options item
+L["has come online"] = "входит в игру." -- pattern
+L["has gone offline"] = "выходит из игрового мира." -- pattern
+L["Hunter"] = "Охотник"  -- class male  
+L["HUNTER"] = "Охотница" -- class female
+L["Mage"] = "Маг" -- class male  
+L["MAGE"] = "Маг" -- class female
+L["Paladin"] = "Паладин" -- class male  
+L["PALADIN"] = "Паладин" -- class female
+L["Priest"] = "Жрец"  -- class male  
+L["PRIEST"] = "Жрица" -- class female
+L["Rogue"] = "Разбойник"  -- class male  
+L["ROGUE"] = "Разбойница" -- class female
+L["Shaman"] = "Шаман"   -- class male  
+L["SHAMAN"] = "Шаманка" -- class female
+L["Tutorial"] = "Строки могут сождержать контектсные тэги, цветовые флаги и прочие символы.\
+Контектсные тэги: &name, &level, &class, &zone, &rank, и &note.\
+Цветовые флаги - :random, :class, :green, :red, :blue, :pink, :cyan, :yellow, и :orange.\
+Флаг :random ведёт себя аналогично правилам Prat-3.0 для 'random'.\
+Например, &name:class быдут выглядеть как %s.\
+Все прочее символы трактуются как часть сообщения." -- description
+L["Warlock"] = "Чернокнижник"  -- class male  
+L["WARLOCK"] = "Чернокнижница" -- class female
+L["Warrior"] = "Воин" -- class male  
+L["WARRIOR"] = "Воин" -- class female
+
