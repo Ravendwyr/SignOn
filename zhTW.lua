a2f6422749995ce3e0ff145199f16039816aa144
Ethan Centaurai <ethan.centaurai@live.co.uk>
2009-05-24 18:23:27 +0100

    Added (incomplete) Traditional Chinese localisation, exported from the Localisation App.

---
+++ zhTW.lua
@@ -0,0 +1,38 @@
+﻿-- Traditional Chinese localization file for zhTW
+local L = LibStub("AceLocale-3.0"):NewLocale("SignOn", "zhTW")
+if not L then return end
+
+L["DEATHKNIGHT"] = "死亡騎士"
+L["DRUID"] = "德魯伊"
+L["Format string for when friends log off"] = "Format string for when friends log off" -- Needs review
+L["Format string for when friends sign on"] = "Format string for when friends sign on" -- Needs review
+L["Format string for when guild members log off"] = "Format string for when guild members log off" -- Needs review
+L["Format string for when guild members sign on"] = "Format string for when guild members sign on" -- Needs review
+L["Friend Log-off Message"] = "好友離線訊息"
+L["Friend Log-on Message"] = "好友上線訊息"
+L["<Friend> &name:random &alts:bracket [&level &class:class] has logged:red off:red &note:bracket"] = "<好友> [&level]&name:class &alts:bracket已經 離線:red了 &note:bracket"
+L["<Friend> &name:random &alts:bracket [&level &class:class] has signed:green on:green in &zone &note:bracket"] = "<好友> [&level]&name:class &alts:bracket已經在&zone 上線:green了 &note:bracket"
+L["Guild Log-off Message"] = "公會成員離線訊息"
+L["Guild Log-on Message"] = "公會成員上線訊息"
+L["<Guild> &rank &name:random &alts:bracket [&level &class:class] has come:green online:green in &zone &note:bracket"] = "<公會> [&level]&name:class &alts:bracket已經在&zone 上線:green了(&rank)"
+L["<Guild> &rank &name:random &alts:bracket [&level &class:class] has logged:red off:red &note:bracket"] = "<公會> [&level]&name:class &alts:bracket已經 離線:red了(&rank)"
+L["has come online"] = "上線了"
+L["(.-) has gone offline"] = "(.-)下線了"
+L["has gone offline"] = "下線了"
+L["|Hplayer:(.-)|h.-|h has come online"] = "|Hplayer:(.-)|h.-|h上線了"
+L["HUNTER"] = "獵人"
+L["MAGE"] = "法師"
+L["PALADIN"] = "聖騎士"
+L["PRIEST"] = "牧師"
+L["ROGUE"] = "盜賊"
+L["SHAMAN"] = "薩滿"
+L["Tutorial"] = [=[Strings can contain tags, flags and any other characters.
+Acceptable tags are &name, &level, &class, &alts, &zone, &rank, and &note.
+Acceptable colour flags are :random, :custom, :class, :green, :red, :blue, :pink, :cyan, :yellow, :white, and :orange.
+Acceptable bracket flags are :bracket, :square, and :angle.
+:random follows the same rules as Prat-3.0's 'random' playername colouring setting.
+:custom is a colour you choose using the swatch below the messages.
+For example, &name:class:square would become %s.
+Anything else will be assumed to be part of the message.]=] -- Needs review
+L["WARLOCK"] = "術士"
+L["WARRIOR"] = "戰士"
